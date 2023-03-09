4b0b98cac7ee6983802018421fe7648d67c71dd8
KANG-KYUNG-MI <112539881+KANG-KYUNG-MI@users.noreply.github.com>
2023-03-09 12:17:47 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+import { addToCart, getTotalCartItems } from "./utils/common.js";
+import { LocalStorage } from "./combo-storage/src/index.js";
+
+const quantityCart = document.querySelector('.quantity-cart');
+const key = "data";
+
+let ls = LocalStorage; // My new API library -.-! 
+
+function getTotalQuantity() {
+    let totalQuantityCart = ls.get(key) ? getTotalCartItems(key) : [];
+    quantityCart.innerText = totalQuantityCart;
+}
+
+function showItems() {
+    axios.get('https://online-shopping-94722-default-rtdb.europe-west1.firebasedatabase.app/items.json')
+        .then(({ data }) => {
+            try {
+                let element = data.map((item) => {
+                    return `
+                    <div class="item ${item.balance < 1 ? "sold-out" : ''}">
+                        <img src="${item.img_url}" alt="", class="item_img" />
+                        <p class="item_name">Name: ${item.name}</p>
+                        <p class="item_price">Price: ${item.price} kr</p>
+                        <button class="add_cart" >Add to cart</button>
+                    </div>
+                  `;
+                }).join('');
+
+                const container = document.querySelector('.items');
+                container.innerHTML = element;
+
+                // attach event listener to the buttons 
+                let btnList = document.querySelectorAll('.add_cart');
+                btnList.forEach((element, btnIndex) => {
+                    element.addEventListener('click', () => {
+                        let payload = data.filter((item, id) => id === btnIndex);
+                        addToCart(key, payload);
+                        getTotalQuantity();
+                    });
+                });
+            } catch (error) {
+                console.error(error);
+            }
+        })
+}
+
+getTotalQuantity()
+showItems();
